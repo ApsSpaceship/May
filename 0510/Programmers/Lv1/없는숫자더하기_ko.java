da71c28cc6be5c251c4ea294d1be63f615e6356c
naryeong-ko <naryung26@gmail.com>
2022-05-09 21:34:29 +0900

    220509_solve : 없는숫자더하기_ko

--- 0510/Programmers/Lv1/없는숫자더하기_ko.java
+++ 0510/Programmers/Lv1/없는숫자더하기_ko.java
@@ -1,1 +1,16 @@
-
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+		boolean[] find = new boolean[10]; // 0~9 사용
+		int len = numbers.length;
+		
+		for (int i = 0; i < len; i++) // 있으면 true
+			find[numbers[i]] = true;
+		
+		for (int i = 0; i < 10; i++) // 없으면 더하기
+			if (!find[i])
+				answer += i;
+		
+		return answer;
+    }
+}
