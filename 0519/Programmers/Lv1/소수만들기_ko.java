000c4eba39f05a72afa1532256585949aea9a776
naryeong-ko <naryung26@gmail.com>
2022-06-02 16:06:21 +0900

    220519_solve : 소수만들기_ko

--- 0519/Programmers/Lv1/소수만들기_ko.java
+++ 0519/Programmers/Lv1/소수만들기_ko.java
@@ -1,1 +1,20 @@
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+		int len = nums.length;
+		for (int i = 0; i < len - 2; i++) // 숫자 3개 합 조합(최소 3)
+			for (int j = i + 1; j < len - 1; j++)
+				for (int k = j + 1; k < len; k++)
+					if (isPrime(nums[i] + nums[j] + nums[k]))
+						answer++;
 
+		return answer;
+    }
+    public static boolean isPrime(int num) { // 소수 판별
+		for (int i = 2; i * i <= num; i++)
+			if (num % i == 0)
+				return false;
+
+		return true;
+	}
+}
