2339edb3577399a14db7fdb17be4840dd94ff4c2
naryeong-ko <naryung26@gmail.com>
2022-06-02 16:05:51 +0900

    220512_solve : 음양더하기_ko

--- 0512/Programmers/Lv1/음양더하기_ko.java
+++ 0512/Programmers/Lv1/음양더하기_ko.java
@@ -1,1 +1,12 @@
+public int solution(int[] absolutes, boolean[] signs) {
+    int answer = 0;
+    int len = signs.length;
+    for (int i = 0; i < len; i++) {
+        if (signs[i]) // 양수면 더하고
+            answer += absolutes[i];
+        else // 음수면 빼기
+            answer -= absolutes[i];
+    }
 
+    return answer;
+}
