cd7476b234d00a60845301b4a5347417c819da6b
naryeong-ko <naryung26@gmail.com>
2022-06-02 16:07:16 +0900

    220526_solve : 풍선터트리기_ko

--- 0526/Programmers/Lv3/풍선터트리기_ko.java
+++ 0526/Programmers/Lv3/풍선터트리기_ko.java
@@ -1,1 +1,20 @@
+import java.util.HashSet;
+class Solution {
 
+    public int solution(int[] a) {
+		HashSet<Integer> set = new HashSet<>();
+		int minL = Integer.MAX_VALUE;
+		int minR = Integer.MAX_VALUE;
+		int len = a.length;
+
+		for (int i = 0; i < len; i++) {
+			minL = Math.min(minL, a[i]);
+			minR = Math.min(minR, a[len - 1 - i]);
+			set.add(minL);
+			set.add(minR);
+		}
+
+		return set.size();
+	}
+
+}
